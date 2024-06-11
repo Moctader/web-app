0269972f048430d37ca73d72a068fff8cf12d29b
Md Golam Moctader <golam.moctader@gmail.com>
2024-06-11 14:25:31 +0300

    tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,13 +7,16 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
+    assert "Product 1" in response.text
 
 def test_add_to_cart():
     response = client.post("/add_to_cart/", data={"product_id": 1})
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
+    assert "Product 1" in response.text
 
 def test_view_cart():
     response = client.get("/cart")
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
+    assert "Total Price" in response.text
